e1cb41bb2473ed48f1d213ea20fdaf75df8ba732
Dan King <dan.king@vptech.io>
2021-09-19 10:41:55 -0700

    Implement basic Flask framework with parameters from .env

---
+++ index.py
@@ -0,0 +1,38 @@
+import os
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    send_from_directory,
+    session,
+    url_for,
+)
+from dotenv import load_dotenv
+
+
+# Configure environment
+load_dotenv()
+print(f"FLASK_APP: {os.environ.get('FLASK_APP')}")
+print(f"FLASK_ENV: {os.environ.get('FLASK_ENV')}")
+print(f"FLASK_DEBUG: {os.environ.get('FLASK_DEBUG')}")
+print(f"FLASK_RUN_HOST: {os.environ.get('FLASK_RUN_HOST')}")
+print(f"FLASK_RUN_PORT: {os.environ.get('FLASK_RUN_PORT')}")
+
+
+# Create Flask app
+app = Flask(__name__)
+
+
+# Start the Flask app if the current, active module is __main__.
+print(f"__name__: {__name__}")
+if __name__ == "__main__":
+    print(
+        f"Running app on host {os.environ.get('FLASK_RUN_HOST')} on port {os.environ.get('FLASK_RUN_PORT')}"
+    )
+    # app.run(debug=True)
+    # app.run()
+    # app.run(port=os.environ.get('FLASK_RUN_PORT'))
+    app.run(
+        host=os.environ.get("FLASK_RUN_HOST"), port=os.environ.get("FLASK_RUN_PORT")
+    )
